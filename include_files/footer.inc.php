f92cb0e8ed27d33c0dd7f86ec418df3a0688504d
Francesco Levanti <francesco@levanti.net>
2015-05-17 14:55:08 +0100

    Added a footer  link to visits page to quickly open it

--- include_files/footer.inc.php
+++ include_files/footer.inc.php
@@ -4,7 +4,7 @@
     echo "<p class=\"text-muted\">";
     echo "Page served id " . round((microtime(true) - $boot_time),4) . " seconds. ";
     echo "Server time " . date("d M Y H:i:s") . " - (" . time() . ")<br>";
-    echo "Developer: Francesco Levanti <a href=\"/bgmonitor\">System Services Status</a> <a href=\"/stats\">Stats</a>";
+    echo "Developer: Francesco Levanti <a href=\"/bgmonitor\">System Services Status</a> <a href=\"/stats\">Stats</a> <a href=\"/infovisits\">Visits</a>";
     echo "<br>";
     echo "DB CONNECTIONS: ";
     if (isset($mydbh)) {

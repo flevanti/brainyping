0f2b17e05e554d5821ea9a720fa8ae578c27b83a
Francesco Levanti <francesco@levanti.net>
2015-06-01 22:02:10 +0100

    Added GitHub link to the footer.

--- include_files/footer.inc.php
+++ include_files/footer.inc.php
@@ -3,6 +3,8 @@ echo "<div class=\"footer\"><div class=\"container\"><p class=\"text-muted\">";
 echo "Page served id " . round((microtime(true) - $boot_time), 4) . " seconds. ";
 echo "Server time " . date("d M Y H:i:s") . " - (" . time() . ")<br>";
 echo "Developer: Francesco Levanti <a href=\"/bgmonitor\">System Services Status</a> <a href=\"/stats\">Stats</a> <a href=\"/infovisits\">Visits</a>";
+echo "&nbsp;&nbsp;&nbsp;";
+echo "<a href=\"https://github.com/flevanti/brainyping\" target='_blank' title=\"Brainyping on GitHub\" alt=\"Brainyping on GitHub\"><img style=\"vertical-align: baseline;\" src=\"imgs/github12x12.png\"></a>";
 echo "<br>";
 echo "DB CONNECTIONS: ";
 if (isset($mydbh)) {
@@ -11,4 +13,7 @@ if (isset($mydbh)) {
 if (isset($mydbh_web)) {
     echo "[web] ";
 }
+
+
+
 echo "</p></div></div>";

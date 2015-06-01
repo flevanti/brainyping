b9cac1348719a6c834d1b7a02f2cd34b4e2ec605
Francesco Levanti <francesco@levanti.net>
2015-06-01 22:07:21 +0100

    Convert GitHub footer logo style to css code instead of inline styling.

--- include_files/footer.inc.php
+++ include_files/footer.inc.php
@@ -4,7 +4,7 @@ echo "Page served id " . round((microtime(true) - $boot_time), 4) . " seconds. "
 echo "Server time " . date("d M Y H:i:s") . " - (" . time() . ")<br>";
 echo "Developer: Francesco Levanti <a href=\"/bgmonitor\">System Services Status</a> <a href=\"/stats\">Stats</a> <a href=\"/infovisits\">Visits</a>";
 echo "&nbsp;&nbsp;&nbsp;";
-echo "<a href=\"https://github.com/flevanti/brainyping\" target='_blank' title=\"Brainyping on GitHub\" alt=\"Brainyping on GitHub\"><img style=\"vertical-align: baseline;\" src=\"imgs/github12x12.png\"></a>";
+echo "<a href=\"https://github.com/flevanti/brainyping\" target='_blank' title=\"Brainyping on GitHub\" alt=\"Brainyping on GitHub\"><img class=\"github_footer_logo\" src=\"imgs/github12x12.png\"></a>";
 echo "<br>";
 echo "DB CONNECTIONS: ";
 if (isset($mydbh)) {
